25fb1d1a050093d43fad697a092ea714b453032c
rickwebiii <rick.weber.iii@gmail.com>
2025-04-10 13:49:50 -0700

    Ryan feedback

--- README.md
+++ README.md
@@ -15,16 +15,7 @@ Program that will run on the Parasol processor:
 
 `add.c`:
 ```C
-#include<stdint.h>
-#include<stdbool.h>
-
-[[clang::fhe_circuit]] void add(
-    [[clang::encrypted]] uint8_t *a,
-    [[clang::encrypted]] uint8_t *b,
-    [[clang::encrypted]] uint8_t *output
-) {
-    *output = *a + *b;
-}
+{{#include examples/basic_add/data/add.c}}
 ```
 
 Compile `add.c`
@@ -36,88 +27,7 @@ This Rust program that runs on the host. This performs encryption, key generatio
 
 `main.rs`
 ```rust
-use parasol_runtime::{Encryption, Evaluation, SecretKey, ComputeKey};
-
-// We know the server program takes three arguments: two input values and an
-// output buffer.
-type AddArguments = [parasol_cpu::Buffer; 3];
-
-// The code the user will run
-fn user_program<F>(compute_add_on_server: F)
-where
-    F: FnOnce(ComputeKey, Encryption, AddArguments) -> parasol_cpu::Buffer,
-{
-    // Generate a secret key for the user. The `DEFAULT_128` parameter is the
-    // security level of the encryption. The higher the number, the more secure
-    // the encryption, but the slower the operations.
-    let secret_key = SecretKey::generate(&parasol_runtime::DEFAULT_128);
-
-    // Generate the encryption parameters for the user
-    let enc = Encryption::new(&parasol_runtime::DEFAULT_128);
-
-    // Generate a server key for the user. These keys are used for operations
-    // and do not give the server access to the data.
-    let server_key = ComputeKey::generate(&secret_key, &parasol_runtime::DEFAULT_128);
-
-    // Define the values we want to add. The sizes of the values must match the
-    // size of the values defined in the C TFHE program!
-    let a = 2u8;
-    let b = 7u8;
-
-    // To pass arguments into the TFHE C program, we must convert them to
-    // `Buffer`s. Note that we must provide an output buffer as well!
-    let arguments = [a, b, 0u8].map(|x| parasol_cpu::Buffer::cipher_from_value(&x, &enc, &secret_key));
-
-    // Call the server using an remote procedure call (RPC) interface.
-    let encrypted_result = compute_add_on_server(server_key, enc.clone(), arguments);
-
-    // Decypt the result.
-    let result = encrypted_result
-        .cipher_try_into_value::<u8>(&enc, &secret_key)
-        .unwrap();
-    println!("Encrypted {a} + {b} = {result}");
-}
-
-// The code the server will run
-use std::sync::Arc;
-use parasol_cpu::{FheApplication, FheComputer};
-
-// Define the path to the compiled TFHE add program.
-const FHE_FILE: &[u8] = include_bytes!("../data/add.a");
-
-fn server_program(
-    server_key: ComputeKey,
-    enc: Encryption,
-    arguments: AddArguments,
-) -> parasol_cpu::Buffer {
-    // Generate a new FHE processor for the server
-    let eval = Evaluation::new(
-        Arc::new(server_key),
-        &parasol_runtime::DEFAULT_128,
-        &enc,
-    );
-    let mut proc = FheComputer::new(&enc, &eval);
-
-    // Load in the TFHE compiled file.
-    let result = FheApplication::parse_elf(FHE_FILE).unwrap();
-
-    // Get the specific function we want to run from the compiled file. Note
-    // that the name "add" in the call to `get_program` has to match the name of
-    // the function in the C file.
-    let add = result.get_program(&"add".into()).unwrap();
-
-    // Run the addition program with the provided encrypted arguments
-    // (a, b, output).
-    proc.run_program(add, &arguments).unwrap();
-
-    // The output parameter is the last one, so we extract that value and return
-    // it to the user.
-    arguments[arguments.len() - 1].clone()
-}
-
-fn main() {
-    user_program(server_program)
-}
+{{#include examples/basic_add/src/main.rs}}
 ```
 
 And finally, our `Cargo.toml`
--- examples/basic_add/src/main.rs
+++ examples/basic_add/src/main.rs
@@ -1,79 +1,59 @@
-use parasol_runtime::{ComputeKey, Encryption, Evaluation, SecretKey};
+use parasol_cpu::{run_program, Buffer};
+use parasol_runtime::{ComputeKey, Encryption, SecretKey};
 
-// We know the server program takes three arguments: two input values and an
-// output buffer.
-type AddArguments = [parasol_cpu::Buffer; 3];
+// Define the path to the compiled TFHE add program.
+const FHE_FILE: &[u8] =
+    include_bytes!("../data/add.a");
 
-// The code the user will run
-fn user_program<F>(compute_add_on_server: F)
-where
-    F: FnOnce(ComputeKey, Encryption, AddArguments) -> parasol_cpu::Buffer,
-{
-    // Generate a secret key for the user. The `DEFAULT_128` parameter is the
-    // security level of the encryption. The higher the number, the more secure
-    // the encryption, but the slower the operations.
-    let secret_key = SecretKey::generate(&parasol_runtime::DEFAULT_128);
+fn main() {
+    // Generate a secret key for the user. By default this ensures
+    // 128 bit security.
+    let secret_key =
+        SecretKey::generate_with_default_params();
 
-    // Generate the encryption parameters for the user
-    let enc = Encryption::new(&parasol_runtime::DEFAULT_128);
+    // Generate a compute key for the user. These keys are used for
+    // operations and do not give access to the plaintext data;
+    // therefore, this key can safely be shared with another party.
+    let compute_key =
+        ComputeKey::generate_with_default_params(
+            &secret_key,
+        );
 
-    // Generate a server key for the user. These keys are used for operations
-    // and do not give the server access to the data.
-    let server_key = ComputeKey::generate(&secret_key, &parasol_runtime::DEFAULT_128);
-
-    // Define the values we want to add. The sizes of the values must match the
-    // size of the values defined in the C TFHE program!
+    // Define the values we want to add. The sizes of the values
+    // must match the size of the values defined in the
+    // C TFHE program!
     let a = 2u8;
     let b = 7u8;
 
-    // To pass arguments into the TFHE C program, we must convert them to
-    // `Buffer`s. Note that we must provide an output buffer as well!
-    let arguments =
-        [a, b, 0u8].map(|x| parasol_cpu::Buffer::cipher_from_value(&x, &enc, &secret_key));
+    // To pass arguments into the TFHE C program, we must convert
+    // them to `Buffer`s. Note that we must provide an output
+    // buffer as well!
+    let arguments = [a, b, 0u8].map(|x| {
+        Buffer::cipher_from_value(
+            &x,
+            &Encryption::default(),
+            &secret_key,
+        )
+    });
 
-    // Call the server using an remote procedure call (RPC) interface.
-    let encrypted_result = compute_add_on_server(server_key, enc.clone(), arguments);
+    // Run the program.
+    let encrypted_result = run_program(
+        compute_key.clone(),
+        FHE_FILE,
+        "add",
+        &arguments,
+    )
+    .unwrap();
 
-    // Decypt the result.
-    let result = encrypted_result
-        .cipher_try_into_value::<u8>(&enc, &secret_key)
+    // Decypt the result. Note that we have to choose the index
+    // to decrypt from all the arguments passed to the C function;
+    // since the result is written out to the third argument of
+    // the `add` function in C, we specify that index here.
+    let result = encrypted_result[2]
+        .cipher_try_into_value::<u8>(
+            &Encryption::default(),
+            &secret_key,
+        )
         .unwrap();
     println!("Encrypted {a} + {b} = {result}");
-}
-
-// The code the server will run
-use parasol_cpu::{FheApplication, FheComputer};
-use std::sync::Arc;
-
-// Define the path to the compiled TFHE add program.
-const FHE_FILE: &[u8] = include_bytes!("../data/add.a");
-
-fn server_program(
-    server_key: ComputeKey,
-    enc: Encryption,
-    arguments: AddArguments,
-) -> parasol_cpu::Buffer {
-    // Generate a new FHE processor for the server
-    let eval = Evaluation::new(Arc::new(server_key), &parasol_runtime::DEFAULT_128, &enc);
-    let mut proc = FheComputer::new(&enc, &eval);
-
-    // Load in the TFHE compiled file.
-    let result = FheApplication::parse_elf(FHE_FILE).unwrap();
-
-    // Get the specific function we want to run from the compiled file. Note
-    // that the name "add" in the call to `get_program` has to match the name of
-    // the function in the C file.
-    let add = result.get_program(&"add".into()).unwrap();
-
-    // Run the addition program with the provided encrypted arguments
-    // (a, b, output).
-    proc.run_program(add, &arguments).unwrap();
-
-    // The output parameter is the last one, so we extract that value and return
-    // it to the user.
-    arguments[arguments.len() - 1].clone()
-}
-
-fn main() {
-    user_program(server_program)
 }
