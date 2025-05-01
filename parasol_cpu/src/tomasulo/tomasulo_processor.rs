f5845d7de4578ffed9683bd5833eb0baffc132cf
rickwebiii <rick.weber.iii@gmail.com>
2025-04-30 20:08:03 -0700

    Remove unused shit

--- parasol_cpu/src/tomasulo/tomasulo_processor.rs
+++ parasol_cpu/src/tomasulo/tomasulo_processor.rs
@@ -1,9 +1,6 @@
-use std::{
-    ops::{Deref, DerefMut},
-    sync::{RwLockReadGuard, RwLockWriteGuard, mpsc::Sender},
-};
+use std::sync::mpsc::Sender;
 
-use super::{registers::RobEntry, scoreboard::ScoreboardEntryRef};
+use super::scoreboard::ScoreboardEntryRef;
 use crate::Result;
 
 #[derive(Clone)]
@@ -31,91 +28,7 @@ pub trait Tomasulo {
     ) -> Result<u32>;
 }
 
-pub trait SelectConstant<T> {
-    fn select_constant_zero(&self) -> &T;
-    fn select_constant_one(&self) -> &T;
-}
-
 pub enum InstructionOperation<I: Clone> {
     Retire(Result<ScoreboardEntryRef<I>>),
     Exec(ScoreboardEntryRef<I>),
-}
-
-pub enum RegRef<'a, T>
-where
-    T: Default,
-{
-    Read(RwLockReadGuard<'a, RobEntry<T>>),
-    Write(RwLockWriteGuard<'a, RobEntry<T>>),
-    Imm(&'a T),
-}
-
-impl<'a, T> From<RwLockReadGuard<'a, RobEntry<T>>> for RegRef<'a, T>
-where
-    T: Default,
-{
-    fn from(value: RwLockReadGuard<'a, RobEntry<T>>) -> Self {
-        Self::Read(value)
-    }
-}
-
-impl<'a, T> From<RwLockWriteGuard<'a, RobEntry<T>>> for RegRef<'a, T>
-where
-    T: Default,
-{
-    fn from(value: RwLockWriteGuard<'a, RobEntry<T>>) -> Self {
-        Self::Write(value)
-    }
-}
-
-impl<'a, T> From<&'a T> for RegRef<'a, T>
-where
-    T: Default,
-{
-    fn from(value: &'a T) -> Self {
-        Self::Imm(value)
-    }
-}
-
-impl<T> Deref for RegRef<'_, T>
-where
-    T: Default,
-{
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        match self {
-            Self::Imm(x) => x,
-            Self::Read(x) => x,
-            Self::Write(x) => x,
-        }
-    }
-}
-
-impl<T> DerefMut for RegRef<'_, T>
-where
-    T: Default,
-{
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        match self {
-            Self::Write(x) => x,
-            _ => unreachable!("Cannot mutate immutable RegRef"),
-        }
-    }
-}
-
-/// An implementation detail of the [`impl_tomasulo`]` macro
-#[macro_export]
-macro_rules! impl_select_constant {
-    ($ty:ident, $ct_ty:ident,$idx:tt) => {
-        impl $crate::tomasulo::tomasulo_processor::SelectConstant<$ct_ty> for $ty {
-            fn select_constant_zero(&self) -> &$ct_ty {
-                &self.zero.$idx
-            }
-
-            fn select_constant_one(&self) -> &$ct_ty {
-                &self.one.$idx
-            }
-        }
-    };
 }
