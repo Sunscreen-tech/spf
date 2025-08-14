987995eb02e2d7c825a1564c22b11dcee6070c45
rickwebiii <rick@sunscreen.tech>
2025-08-14 18:38:34 +0000

    Revert a change

--- sunscreen_math_macros/src/lib.rs
+++ sunscreen_math_macros/src/lib.rs
@@ -158,13 +158,7 @@ pub fn derive_barrett_config(input: proc_macro::TokenStream) -> TokenStream {
 /// use sunscreen_math_macros::refify_binary_op;
 ///
 /// pub trait WrappingSemantics:
-///     Copy +
-///     Clone +
-///     std::fmt::Debug +
-///     WrappingAdd<WrappingOutput = Self> +
-///     WrappingMul<WrappingOutput = Self> +
-///     WrappingSub<WrappingOutput = Self> +
-///     WrappingNeg<WrappingOutput = Self>
+///     Copy + Clone + std::fmt::Debug + WrappingAdd + WrappingMul + WrappingSub + WrappingNeg
 /// {
 /// }
 ///
@@ -189,7 +183,7 @@ pub fn derive_barrett_config(input: proc_macro::TokenStream) -> TokenStream {
 ///     type Output = ZInt<T>;
 ///
 ///     fn add(self, rhs: &ZInt<T>) -> Self::Output {
-///         ZInt(self.0.wrapping_add(rhs.0))
+///         ZInt(self.0.wrapping_add(&rhs.0))
 ///     }
 /// }
 /// ```
