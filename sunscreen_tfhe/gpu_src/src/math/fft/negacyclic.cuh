9d9b8834b033533c4e60479a25fb4339876548e8
rickwebiii <rick@sunscreen.tech>
2025-08-20 22:29:25 -0700

    WIP

--- sunscreen_tfhe/gpu_src/src/math/fft/negacyclic.cuh
+++ sunscreen_tfhe/gpu_src/src/math/fft/negacyclic.cuh
@@ -1,52 +1,53 @@
 #pragma once
+#include <cuda/std/complex>
 #include <cstdint>
 
 #include "twiddles.cuh"
+#include "../entities/punbuf.cuh"
 #include "../math.cuh"
+#include "../primitives.cuh"
 #include "./fft.cuh"
 #include "../../iter_tools.cuh"
 
-template <typename T>
-__device__ inline Complex<T>* apply_twist(
-    T* s_input,
+__device__ inline apply_twist(
+    PunBuf punbuf,
     u32 n
 ) {
     static_assert(2 * sizeof(T) == sizeof(Complex<T>));
     
     u32 n_times_2 = n * 2;
     u32 n_div_2 = n / 2;
-    auto s_output = reinterpret_cast<Complex<T>*>(s_input);
-
     u32 tid = threadIdx.x;
     u32 dim = blockDim.x;
+
+    auto as_f64 = punbuf.as_f64();
 
     // Since negacyclic FFT requires n / 8 threads in a block, we have each thread load their values 
     // into registers so we can write them back to shared memory in a coordinated manner.
     //
     // Our input is over T, but we're emitting an output over Complex<T>. As such, each thread needs to
     // produce 4 complex points (which consumes 8 values in the original input array).
-    auto c0 = Complex<T>(s_input[tid + 0 * dim], s_input[tid + n_div_2 + 0 * dim]);
-    auto c1 = Complex<T>(s_input[tid + 1 * dim], s_input[tid + n_div_2 + 1 * dim]);
-    auto c2 = Complex<T>(s_input[tid + 2 * dim], s_input[tid + n_div_2 + 2 * dim]);
-    auto c3 = Complex<T>(s_input[tid + 3 * dim], s_input[tid + n_div_2 + 3 * dim]);
+    auto c0 = cuda::std::complex<f64>(as_f64[tid + 0 * dim], as_f64[tid + n_div_2 + 0 * dim]);
+    auto c1 = cuda::std::complex<f64>(as_f64[tid + 1 * dim], as_f64[tid + n_div_2 + 1 * dim]);
+    auto c2 = cuda::std::complex<f64>(as_f64[tid + 2 * dim], as_f64[tid + n_div_2 + 2 * dim]);
+    auto c3 = cuda::std::complex<f64>(as_f64[tid + 3 * dim], as_f64[tid + n_div_2 + 3 * dim]);
     
     // Ensure all our values have been loaded into registers.
     __syncthreads();
 
+    auto as_complex = punbuf.as_complex();
+
     // Now write our complex registers back to shared memory.
-    s_output[tid + 0 * dim] = c0 * FftTwiddles<T>::Get_W_value_inverse(n_times_2, tid + 0 * dim);
-    s_output[tid + 1 * dim] = c1 * FftTwiddles<T>::Get_W_value_inverse(n_times_2, tid + 1 * dim);
-    s_output[tid + 2 * dim] = c2 * FftTwiddles<T>::Get_W_value_inverse(n_times_2, tid + 2 * dim);
-    s_output[tid + 3 * dim] = c3 * FftTwiddles<T>::Get_W_value_inverse(n_times_2, tid + 3 * dim);
+    as_complex[tid + 0 * dim] = c0 * FftTwiddles<T>::Get_W_value_inverse(n_times_2, tid + 0 * dim);
+    as_complex[tid + 1 * dim] = c1 * FftTwiddles<T>::Get_W_value_inverse(n_times_2, tid + 1 * dim);
+    as_complex[tid + 2 * dim] = c2 * FftTwiddles<T>::Get_W_value_inverse(n_times_2, tid + 2 * dim);
+    as_complex[tid + 3 * dim] = c3 * FftTwiddles<T>::Get_W_value_inverse(n_times_2, tid + 3 * dim);
 
     __syncthreads();
-
-    return s_output;
 }
 
-template <typename T>
 __device__ inline T* remove_twist(
-    Complex<T>* s_input,
+    PunBuf punbuf,
     u32 n
 ) {
     static_assert(2 * sizeof(T) == sizeof(Complex<T>));
@@ -55,72 +56,41 @@ __device__ inline T* remove_twist(
     u32 n_div_2 = n / 2;
     T n_inv = 1.0 / (T)n_div_2;
 
-    auto s_output = reinterpret_cast<T*>(s_input);
+    auto as_complex = punbuf.as_complex();
 
     u32 tid = threadIdx.x;
     u32 dim = blockDim.x;
 
     // As with twisting, we load 4 Complex values into registers and then synchronize so we can overwrite our input buffer.
-    auto c0 = s_input[tid + 0 * dim] * FftTwiddles<T>::Get_W_value(n_times_2, tid + 0 * dim) * n_inv;
-    auto c1 = s_input[tid + 1 * dim] * FftTwiddles<T>::Get_W_value(n_times_2, tid + 1 * dim) * n_inv;
-    auto c2 = s_input[tid + 2 * dim] * FftTwiddles<T>::Get_W_value(n_times_2, tid + 2 * dim) * n_inv;
-    auto c3 = s_input[tid + 3 * dim] * FftTwiddles<T>::Get_W_value(n_times_2, tid + 3 * dim) * n_inv;
+    auto c0 = as_complex[tid + 0 * dim] * FftTwiddles<T>::Get_W_value(n_times_2, tid + 0 * dim) * n_inv;
+    auto c1 = as_complex[tid + 1 * dim] * FftTwiddles<T>::Get_W_value(n_times_2, tid + 1 * dim) * n_inv;
+    auto c2 = as_complex[tid + 2 * dim] * FftTwiddles<T>::Get_W_value(n_times_2, tid + 2 * dim) * n_inv;
+    auto c3 = as_complex[tid + 3 * dim] * FftTwiddles<T>::Get_W_value(n_times_2, tid + 3 * dim) * n_inv;
 
     __syncthreads();
 
-    s_output[tid + 0 * dim] = round(c0.re());
-    s_output[tid + 0 * dim + n_div_2] = round(c0.im());
-    s_output[tid + 1 * dim] = round(c1.re());
-    s_output[tid + 1 * dim + n_div_2] = round(c1.im());
-    s_output[tid + 2 * dim] = round(c2.re());
-    s_output[tid + 2 * dim + n_div_2] = round(c2.im());
-    s_output[tid + 3 * dim] = round(c3.re());
-    s_output[tid + 3 * dim + n_div_2] = round(c3.im());
+    auto as_f64 = punbuf.as_f64();
+
+    as_f64[tid + 0 * dim] = round(c0.re());
+    as_f64[tid + 0 * dim + n_div_2] = round(c0.im());
+    as_f64[tid + 1 * dim] = round(c1.re());
+    as_f64[tid + 1 * dim + n_div_2] = round(c1.im());
+    as_f64[tid + 2 * dim] = round(c2.re());
+    as_f64[tid + 2 * dim + n_div_2] = round(c2.im());
+    as_f64[tid + 3 * dim] = round(c3.re());
+    as_f64[tid + 3 * dim + n_div_2] = round(c3.im());
 
     __syncthreads();
 
     return s_output;
-}
-
-/// Compute a forward FFT over real negacyclic input `s_input`.
-template <typename T>
-__device__ Complex<T>* twisted_fft(
-    T *s_input,
-    u32 n)
-{
-    u32 n_div_2 = n / 2;
-    T n_inv = 1.0 / (T)n_div_2;
-
-    auto s_output = apply_twist(s_input, n);
-
-    // Perform an n/2 FFT.
-    fft(s_output, n_div_2);
-
-    return s_output;
-}
-
-/// Compute a twisted IFFT resulting in real negacyclic output `s_output`.
-/// This result is still in floating point and needs to be modulo reduced.
-template <typename T>
-__device__ T* twisted_ifft(
-    Complex<T> *s_input,
-    u32 n)
-{
-    u32 n_div_2 = n / 2;
-
-    // Perform an n/2 IFFT.
-    ifft(s_input, n_div_2);
-
-    return remove_twist(s_input, n);
 }
 
 /// Compute a forward in-place FFT over real negacyclic input `s_input`. For best performance,
 /// s_input should be shared memory.
 ///
-/// The returned pointer is the input buffer re-interpreted as `Complex<T>`
-template <typename T>
+/// Upon completion, the complex
 __device__ Complex<T>* twisted_fft_noreorder(
-    T *s_input,
+    PunBuf s_input,
     u32 n)
 {
     u32 n_div_2 = n / Float<T>::TWO;
@@ -137,7 +107,7 @@ __device__ Complex<T>* twisted_fft_noreorder(
 /// This result is still in floating point and needs to be modulo reduced.
 template <typename T>
 __device__ T* twisted_ifft_noreorder(
-    Complex<T> *s_input,
+    PunBuf s_input,
     u32 n)
 {
     u32 n_div_2 = n / 2;
--- sunscreen_tfhe/gpu_src/tests/ops/polynomial.cuh
+++ sunscreen_tfhe/gpu_src/tests/ops/polynomial.cuh
@@ -1,4 +1,6 @@
 #pragma once
+#include <cuda/std/complex>
+
 #include "../../src/entities/dst_array.cuh"
 #include "../../src/entities/polynomial.cuh"
 #include "../../src/entities/scratch.cuh"
@@ -6,154 +8,174 @@
 #include "../../src/params.cuh"
 
 extern "C" __global__ void can_polynomial_rountrip_fft(
-    const DstArray<Polynomial<uint64_t>> *__restrict__ x,
-    DstArray<Polynomial<uint64_t>> *__restrict__ y,
-    uint8_t *scratch_buf,
+    const cuda::std::complex<f64> *__restrict__ x_buf,
+    cuda::std::complex<f64> *__restrict__ y_buf,
     const u32 n)
 {
+    auto x = DstArray<Polynomial>::from_ptr(x_buf);
+    auto y = DstArray<Polynomial>::from_ptr(y_buf);
+    
     auto degree = PolynomialDegree(n);
-    auto allocator = PerBlockStackAllocator(scratch_buf, get_scratch_size());
-    auto tmp = allocator.alloc<PolynomialFft<Complex<double>>>(degree);
-    auto x_i = x->nth(blockIdx.x, degree);
-    auto y_i = y->nth(blockIdx.x, degree);
+    auto tmp = PolynomialFft(get_fft_scratch());
+    auto x_i = x.nth(blockIdx.x, degree);
+    auto y_i = y.nth(blockIdx.x, degree);
 
-    x_i->fft(*tmp, degree);
-    tmp->ifft(y_i, degree);
+    x_i.fft(tmp, degree);
+    tmp.ifft(y_i, degree);
 }
 
 extern "C" __global__ void can_polynomial_rountrip_fft_inplace(
-    const DstArray<Polynomial<uint64_t>> *__restrict__ x,
-    DstArray<Polynomial<uint64_t>> *__restrict__ y,
-    uint8_t *scratch_buf, // Unused, but want to match interface of can_polynomial_rountrip_fft
+    const cuda::std::complex<f64> *__restrict__ x_buf,
+    cuda::std::complex<f64> *__restrict__ y_buf,
     const u32 n)
 {
+    auto x = DstArray<Polynomial>::from_ptr(x_buf);
+    auto y = DstArray<Polynomial>::from_ptr(y_buf);
+
     auto degree = PolynomialDegree(n);
-    auto x_i = x->nth(blockIdx.x, degree);
-    auto y_i = y->nth(blockIdx.x, degree);
+    auto x_i = x.nth(blockIdx.x, degree);
+    auto y_i = y.nth(blockIdx.x, degree);
 
-    auto s_in = get_fft_scratch<Polynomial<uint64_t>>();
+    auto s_in = Polynomial(get_fft_scratch());
 
-    BLOCK_COPY(s_in->coeffs(), x_i->coeffs(), n);
+    x_i.clone_into(s_in, degree);
 
-    auto s_fft = s_in->fft_inplace<Complex<double>>(degree);
-    auto s_out = s_fft->ifft_inplace<uint64_t>(degree);
+    auto s_fft = s_in.fft_inplace(degree);
+    auto s_out = s_fft.ifft_inplace(degree);
 
-    BLOCK_COPY(y_i->coeffs(), s_out->coeffs(), n);
+    s_out.clone_into(y_i, degree);
 }
 
 extern "C" __global__ void can_sub_polynomials(
-    DstArray<Polynomial<uint64_t>> *c,
-    const DstArray<Polynomial<uint64_t>> *a,
-    const DstArray<Polynomial<uint64_t>> *b,
+    cuda::std::complex<f64> *c_buf,
+    const cuda::std::complex<f64> *a_buf,
+    const cuda::std::complex<f64> *b_buf,
     u32 d)
 {
     auto degree = PolynomialDegree(d);
+    auto c = DstArray<Polynomial>::from_ptr(c_buf);
+    auto a = DstArray<Polynomial>::from_ptr(a_buf);
+    auto b = DstArray<Polynomial>::from_ptr(b_buf);
 
-    auto c_i = c->nth(blockIdx.x, degree);
-    auto a_i = a->nth(blockIdx.x, degree);
-    auto b_i = b->nth(blockIdx.x, degree);
+    auto c_i = c.nth(blockIdx.x, degree);
+    auto a_i = a.nth(blockIdx.x, degree);
+    auto b_i = b.nth(blockIdx.x, degree);
 
     polynomial_sub(c_i, a_i, b_i, degree);
 }
 
 extern "C" __global__ void can_add_polynomials(
-    DstArray<Polynomial<uint64_t>> *c,
-    const DstArray<Polynomial<uint64_t>> *a,
-    const DstArray<Polynomial<uint64_t>> *b,
+    cuda::std::complex<f64> *c_buf,
+    const cuda::std::complex<f64> *a_buf,
+    const cuda::std::complex<f64> *b_buf,
     u32 d)
 {
     auto degree = PolynomialDegree(d);
+    auto c = DstArray<Polynomial>::from_ptr(c_buf);
+    auto a = DstArray<Polynomial>::from_ptr(a_buf);
+    auto b = DstArray<Polynomial>::from_ptr(b_buf);
 
-    auto c_i = c->nth(blockIdx.x, degree);
-    auto a_i = a->nth(blockIdx.x, degree);
-    auto b_i = b->nth(blockIdx.x, degree);
+    auto c_i = c.nth(blockIdx.x, degree);
+    auto a_i = a.nth(blockIdx.x, degree);
+    auto b_i = b.nth(blockIdx.x, degree);
 
     polynomial_add(c_i, a_i, b_i, degree);
 }
 
 extern "C" __global__ void can_mad_polynomials_pre_fftd(
-    DstArray<PolynomialFft<Complex<double>>> *__restrict__ c_fft,
-    const DstArray<PolynomialFft<Complex<double>>> *__restrict__ a_fft,
-    const DstArray<PolynomialFft<Complex<double>>> *__restrict__ b_fft,
+    cuda::std::complex<f64> *__restrict__ c_fft_buf,
+    const cuda::std::complex<f64> *__restrict__ a_fft_buf,
+    const cuda::std::complex<f64> *__restrict__ b_fft_buf,
     u32 d)
 {
     auto degree = PolynomialDegree(d);
+    auto c_fft = DstArray<PolynomialFft>::from_ptr(c_fft_buf);
+    auto a_fft = DstArray<PolynomialFft>::from_ptr(a_fft_buf);
+    auto b_fft = DstArray<PolynomialFft>::from_ptr(b_fft_buf);
 
-    auto c_i_fft = c_fft->nth(blockIdx.x, degree);
-    auto a_i_fft = a_fft->nth(blockIdx.x, degree);
-    auto b_i_fft = b_fft->nth(blockIdx.x, degree);
+    auto c_i_fft = c_fft.nth(blockIdx.x, degree);
+    auto a_i_fft = a_fft.nth(blockIdx.x, degree);
+    auto b_i_fft = b_fft.nth(blockIdx.x, degree);
 
     polynomial_mad(c_i_fft, a_i_fft, b_i_fft, degree);
 }
 
 extern "C" __global__ void can_multiply_non_negacyclic_polynomials(
-    DstArray<Polynomial<Complex<double>>> *__restrict__ c,
-    const DstArray<Polynomial<Complex<double>>> *__restrict__ a,
-    const DstArray<Polynomial<Complex<double>>> *__restrict__ b,
+    cuda::std::complex<f64> *__restrict__ c_buf,
+    const cuda::std::complex<f64> *__restrict__ a_buf,
+    const cuda::std::complex<f64> *__restrict__ b_buf,
     const u32 d)
 {
     auto degree = PolynomialDegree(d);
 
-    __shared__ Complex<double> a_s[1024];
-    __shared__ Complex<double> b_s[1024];
-    __shared__ Complex<double> c_s[1024];
+    auto a = DstArray<Polynomial>::from_ptr(a_buf);
+    auto b = DstArray<Polynomial>::from_ptr(b_buf);
+    auto c = DstArray<Polynomial>::from_ptr(c_buf);
 
-    auto a_i = a->nth(blockIdx.x, degree);
-    auto b_i = b->nth(blockIdx.x, degree);
-    auto c_i = c->nth(blockIdx.x, degree);
+    auto a_i = a.nth(blockIdx.x, degree);
+    auto b_i = b.nth(blockIdx.x, degree);
+    auto c_i = c.nth(blockIdx.x, degree);
 
-    BLOCK_COPY(a_s, a_i->coeffs(), d);
-    BLOCK_COPY(b_s, b_i->coeffs(), d);
+    auto p_shared = DstArray<Polynomial>(get_fft_scratch());
+    auto a_s = p_shared.nth(0, degree);
+    auto b_s = p_shared.nth(1, degree);
+    auto c_s = p_shared.nth(2, degree);
 
-    fft_noreorder(a_s, d);
-    fft_noreorder(b_s, d);
+    a_i.clone_into(a_s, d);
+    b_i.clone_into(b_s, d);
+
+    fft_noreorder(a_s.coeffs().as_complex(), d);
+    fft_noreorder(b_s.coeffs().as_complex(), d);
 
     BLOCK_FOR_EACH(i, d)
     {
-        c_s[i] = a_s[i] * b_s[i];
+        c_s.coeffs().as_complex()[i] = a_s.coeffs().as_complex()[i] * b_s.coeffs().as_complex()[i];
     }
 
     __syncthreads();
 
-    ifft_noreorder(c_s, d);
+    ifft_noreorder(c_s.coeffs().as_complex(), d);
 
-    BLOCK_COPY(c_i->coeffs(), c_s, d);
+    c_s.clone_into(c_i, d);
 }
 
 extern "C" __global__ void can_mad_polynomials(
-    DstArray<Polynomial<double>> *__restrict__ result,
-    DstArray<Polynomial<uint64_t>> *__restrict__ c,
-    const DstArray<Polynomial<uint64_t>> *__restrict__ a,
-    const DstArray<Polynomial<uint64_t>> *__restrict__ b,
-    uint8_t *scratch_buf,
+    cuda::std::complex<f64> *__restrict__ result,
+    cuda::std::complex<f64> *__restrict__ c_buf,
+    const cuda::std::complex<f64> *__restrict__ a_buf,
+    const cuda::std::complex<f64> *__restrict__ b_buf,
     u32 d)
 {
     auto degree = PolynomialDegree(d);
     auto scratch = PerBlockStackAllocator(scratch_buf, get_scratch_size());
 
-    auto c_i = c->nth(blockIdx.x, degree);
-    auto a_i = a->nth(blockIdx.x, degree);
-    auto b_i = b->nth(blockIdx.x, degree);
+    auto c = DstArray<Polynomial>::from_ptr(c_buf);
+    auto a = DstArray<Polynomial>::from_ptr(a_buf);
+    auto b = DstArray<Polynomial>::from_ptr(b_buf);
+    auto c_i = c.nth(blockIdx.x, degree);
+    auto a_i = a.nth(blockIdx.x, degree);
+    auto b_i = b.nth(blockIdx.x, degree);
 
-    auto c_i_fft = scratch.alloc<PolynomialFft<Complex<double>>>(d);
-    auto a_i_fft = scratch.alloc<PolynomialFft<Complex<double>>>(d);
-    auto b_i_fft = scratch.alloc<PolynomialFft<Complex<double>>>(d);
+    auto p_shared = DstArray<PolynomialFft>(get_fft_scratch());
+    auto c_i_fft = p_shared.nth(0, d);
+    auto a_i_fft = p_shared.nth(1, d);
+    auto b_i_fft = p_shared.nth(2, d);
 
-    c_i->fft(*c_i_fft, degree);
-    a_i->fft(*a_i_fft, degree);
-    b_i->fft(*b_i_fft, degree);
+    c_i.fft(c_i_fft, degree);
+    a_i.fft(a_i_fft, degree);
+    b_i.fft(b_i_fft, degree);
 
-    polynomial_mad(*c_i_fft, *a_i_fft, *b_i_fft, degree);
+    polynomial_mad(c_i_fft, a_i_fft, b_i_fft, degree);
 
     // Set the modulo-reduced result
-    c_i_fft->ifft(c_i, degree);
+    c_i_fft.ifft(c_i, degree);
     PolynomialDegree n_div_2 = PolynomialDegree{degree.val / 2};
 
     auto s_in = get_fft_scratch<Complex<double>>();
     BLOCK_COPY(s_in, c_i_fft->coeffs(), n_div_2.val);
 
     // Compute the non modulo-reduced result so we can check it as well in our test.
-    auto s_out = twisted_ifft_noreorder(s_in, degree.val);
+    twisted_ifft_noreorder(s_in, degree.val);
 
     auto result_i = result->nth(blockIdx.x, degree);
     BLOCK_COPY(result_i->coeffs(), s_out, degree.val);
